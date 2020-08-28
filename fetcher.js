13ddb69786acef72c8cc48ee32bdbfbc9d4f6a8e
Maria Khandaker <mariakhandaker@Marias-MacBook-Pro.local>
2020-08-27 21:21:30 -0400

    finished fetcher exercise

--- fetcher.js
+++ fetcher.js
@@ -4,12 +4,22 @@ const fs = require('fs');
 //process.argv arguments URL and local file path and downloads the resource to the specified path 
 const url = process.argv[2];
 const filePath = process.argv[3];
+const fileName = `${url.split('.')[1]}.txt`
+
+function getFileSize() {
+  const stats = fs.statSync(fileName)
+  return stats.size;
+}
 
 request(url, (error, response, body) => {
   if (error) console.log('error:', error); // Print the error if one occurred
   if (response.statusCode !== 200) console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
- 
-  fs.writeFile(file, data[, options], callback) console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log("hello!"); 
+  fs.writeFile(fileName, body, (err) => {
+    if (err) throw err;
+    const fileSize = getFileSize(fileName);
+    console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}`); // Print the H
+  }) 
 });
 
 
